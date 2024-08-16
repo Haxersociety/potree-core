0e0970b1b90096f3eca53d5bb96d8299080fcf22
Костылев Леонид Андреевич <la_kostylev@controlsystems.ru>
2024-08-16 09:43:11 +0300

    fix SSR

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,36 @@
-import path from 'path';
+const path = require('path')
 
-export default {
-	entry: './source/index.ts',
-	stats: {
-		children: true,
-	},
-	experiments: {
-		outputModule: true,
-	},
-	output: {
-		path: path.resolve('dist'),
-		filename: 'index.js',
-		library: {
-			type: 'module',
-		},
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	plugins: [],
-	externals: ['three'],
-	module: {
-		rules: [
-			{
-				test: /\.worker\.js$/,
-				loader: 'worker-loader',
-				options: {inline: 'no-fallback'},
-			},
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.(vs|fs)$/,
-				loader: 'raw-loader',
-				options: {
-					esModule: true,
-				},
-			}
-		],
-	},
-};
+module.exports = {
+  entry: './source/index.ts',
+  stats: {
+    children: true,
+  },
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
+    library: {
+      type: 'commonjs-module',
+    },
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [],
+  externals: ['three'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(vs|fs)$/,
+        loader: 'raw-loader',
+      },
+    ],
+  },
+}
